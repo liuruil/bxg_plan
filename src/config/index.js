d12725d09c46d75ebbfedc19bddb928a897634b9
httliurui <2223015815@qq.com>
2023-06-23 18:01:45 +0800

    学生计划平台

--- src/config/index.js
+++ src/config/index.js
@@ -20,7 +20,7 @@ module.exports = {
   baseUploadImageUrl: path.resolve(__dirname, "../../public/images"),
   // 浏览器ws地址
   browserWSEndpoint:
-    "ws://localhost:9222/devtools/browser/69f904ff-a135-48d1-aed7-29bd070d70e8",
+    "ws://localhost:9222/devtools/browser/b0327740-897e-45b8-a0ab-2ce5cacf8e82",
   // 页面基地址
   baseUrl:
     "https://work.boxuegu.com/education/my-course-manage/my-course/learning-plan",
--- src/entry.js
+++ src/entry.js
@@ -1,9 +1,11 @@
-// const runAll = require("npm-run-all");
 const chalk = require("chalk");
 const inquirer = require("inquirer");
 const createServer = require("./server");
 const { initSystemConfig, init } = require("./util");
 const { sendCode, login, autoProcess } = require("./script/login");
+
+const { exec } = require("child_process");
+const { start } = require("repl");
 (async () => {
   // 1.初始化配置
   await initSystemConfig();
@@ -11,21 +13,25 @@ const { sendCode, login, autoProcess } = require("./script/login");
   console.log(chalk.blue("🚀 开始执行登录程序"));
   console.log("\r");
   // 3. 发送验证码
-  let { browser, page } = await sendCode();
-  // 4. 输入验证码
-  const { code } = await inquirer.prompt([
-    {
-      type: "input",
-      name: "code",
-      message: "请输入验证码: ",
-    },
-  ]);
-  // 5. 执行登录操作
-  await login(code, page);
-  // 6. 执行自动化操作
-  await autoProcess(page, browser);
-  // 7. 获取学生截图数据
-  await init();
-  // 8. 开启服务器,自动打开我的系统页面
-  createServer();
+  try {
+    let { browser, page } = await sendCode();
+    // 4. 输入验证码
+    const { code } = await inquirer.prompt([
+      {
+        type: "input",
+        name: "code",
+        message: "请输入验证码: ",
+      },
+    ]);
+    // 5. 执行登录操作
+    await login(code, page);
+    // 6. 执行自动化操作
+    await autoProcess(page, browser);
+    // 7. 获取学生截图数据
+    await init();
+    // 8. 开启服务器,自动打开我的系统页面
+    createServer();
+  } catch (error) {
+    console.log(`请重新运行此命令 \r\n${chalk.yellow("yarn start")}`);
+  }
 })();
